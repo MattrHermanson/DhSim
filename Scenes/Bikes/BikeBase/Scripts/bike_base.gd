abef25e2b4cd0735376e8ac0be25129ad3f49251
Matt Hermanson <matth2004@outlook.com>
2025-08-28 16:07:05 -0700

    Added steering force, and steering interpolation

--- Scenes/Bikes/BikeBase/Scripts/bike_base.gd
+++ Scenes/Bikes/BikeBase/Scripts/bike_base.gd
@@ -5,10 +5,10 @@ class_name BikeBase
 @export var rear_wheel : BikeWheelBase
 var wheels: Array[BikeWheelBase]
 
-var pedal_input = 0
-var steering_input = 0
-var front_brake_input = 0
-var rear_brake_input = 0
+var pedal_input := 0.0
+var steering_input := 0.0
+var front_brake_input := 0.0
+var rear_brake_input := 0.0
 
 func _ready() -> void:
 		assert(front_wheel != null, "ERROR: 'front_wheel' must not be null!")
@@ -21,19 +21,71 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 
 func _process(delta: float) -> void:
-	DebugDraw3D.draw_box((global_position + center_of_mass + Vector3(0, 0, 0)), global_transform.basis, Vector3(0.25, 0.25, 0.25), Color.GREEN_YELLOW, true) # draw center of mass
-	pass
+	DebugDraw3D.draw_box((global_position + center_of_mass + Vector3(0, 0, 0)), global_transform.basis, Vector3(0.1, 0.1, 0.1), Color.GREEN_YELLOW, true) # draw center of mass
+	steering_input = Input.get_axis("SteerLeft", "SteerRight")
+	pedal_input = Input.get_action_strength("Pedal")
+	front_brake_input = Input.get_action_strength("FrontBrake")
+	rear_brake_input = Input.get_action_strength("RearBrake")
 	
-		
+	
 func _physics_process(delta: float) -> void:
+	var torque := get_stabilizing_torque()
+	#apply_torque(torque)
+	
 	for wheel in wheels:
 		if wheel.is_colliding():
 			var velocity_at_contact = _get_point_velocity(wheel.get_collision_point())
-			var force_vector = wheel.get_forces(velocity_at_contact)
+			var force_vector = wheel.get_forces(pedal_input, interpolate_steering(steering_input, delta), front_brake_input, rear_brake_input, velocity_at_contact)
 			var force_pos_offset := wheel.get_collision_point() - global_position
 			apply_force(force_vector, force_pos_offset)
-			DebugDraw3D.draw_arrow_ray(wheel.get_collision_point(), force_vector, force_vector.length()*0.0005, Color.SKY_BLUE, 0.1)
 
 # Helper function to get velocity at point
 func _get_point_velocity(point: Vector3) -> Vector3:
 	return linear_velocity + angular_velocity.cross(point - global_position)
+
+# Interesting idea... would probably rather interpolate steering inputs instead of this
+func get_stabilizing_torque() -> Vector3:
+	var speed_threshold := 0.5
+	var roll_damping_rate := 500.0
+	
+	if linear_velocity.dot(-global_basis.z) > speed_threshold:
+		roll_damping_rate = 200.0
+	
+	var roll_angle := global_rotation.z
+	if is_zero_approx(roll_angle):
+		roll_angle = 0.0
+	
+	var roll_correction_torque := roll_angle * roll_damping_rate
+	return Vector3(0, 0, roll_correction_torque)
+
+# TEMP Variables
+var max_lean_angle := 25.0
+var last_error := 0.0
+
+# takes steering_input as a "lean input" and converts that to a steering input for the front wheel
+func interpolate_steering(steering_input: float, delta: float) -> float:
+	
+	var P := 10.0
+	var I := 0.0
+	var D := 1.0
+	
+	var roll_angle := rad_to_deg(global_rotation.z)
+	if is_zero_approx(roll_angle):
+		roll_angle = 0.0
+	
+	var target_lean_angle := steering_input * max_lean_angle
+	var error := -target_lean_angle - roll_angle
+	
+	var p_term := -error * P
+	var i_term := 0.0
+	var d_term := ((-error - last_error) * delta) * D
+	
+	var lean_torque := p_term + d_term
+	last_error = error
+	
+	print("lean t ", lean_torque, " p_term ", p_term, " d_term ", d_term)
+	
+	apply_torque(Vector3(0, 0, lean_torque))
+	
+	# TODO think of a smarter way to add steering, maybe make it scale with speed
+	return steering_input * 0.5
--- Scenes/Bikes/BikeBase/Scripts/bike_wheel_base.gd
+++ Scenes/Bikes/BikeBase/Scripts/bike_wheel_base.gd
@@ -6,30 +6,146 @@ enum WheelType {
 	REAR,
 }
 
-# Wheel Settings
+# Wheel Settings - TODO make conditional http://kehomsforge.com/tutorials/single/conditionally-export-properties-godot/
 @export var spring_strength := 100.0
 @export var spring_damping := 2.0
 @export var wheel_radius := 0.4
+@export var pedal_force := 200
+@export var brake_force := 500
+@export var head_tube_angle := 63.5
+@export var max_steering_angle := 45.0
 @export var wheel_type : WheelType
 @onready var wheel: Node3D = get_child(0) # reference to wheel mesh
+
+var current_steering_angle := 0.0
+var steering_axis : Vector3
 
 func _ready() -> void:
 	if not target_position.y == -(wheel_radius + 0.05):
 		push_warning("Target position not set correctly and won't reflect actual raycast")
 
-func get_forces(velocity: Vector3) -> Vector3:
+	set_steering_axis(deg_to_rad(head_tube_angle))
+
+func get_forces(pedal_input: float, steering_input: float, front_brake_input: float, rear_brake_input: float, velocity: Vector3) -> Vector3:
 	var total_force_vector: Vector3
+	
+	# Add all wheel forces
 	total_force_vector += get_normal_force(velocity)
+	
+	# Add rear only forces
+	if wheel_type == WheelType.REAR:
+		total_force_vector += get_pedal_force(pedal_input)
+		total_force_vector += get_brake_force(rear_brake_input, velocity)
+		total_force_vector += get_steering_force(0, velocity)
+	
+	# Add front only forces
+	if wheel_type == WheelType.FRONT:
+		total_force_vector += get_brake_force(front_brake_input, velocity)
+		total_force_vector += get_steering_force(steering_input, velocity)
+	
+	update_animation(velocity)
 	return total_force_vector
+
+
+func update_animation(velocity: Vector3) -> void:
+	var forward_direction := -global_basis.z
+	var forward_velocity := forward_direction.dot(velocity)
+	wheel.rotate_x((-forward_velocity * get_process_delta_time()) / wheel_radius)
 
 
 func get_normal_force(velocity: Vector3) -> Vector3:
 	target_position.y = -(wheel_radius + 0.05) # wheel_radius + magic offset, maybe remove
 	var contact := get_collision_point()
-	var spring_up_dir := global_transform.basis.y
+	var spring_up_direction := global_transform.basis.y
 	var penetration := wheel_radius - global_position.distance_to(contact)
-	#wheel.position.y = -spring_len
 	var spring_force := spring_strength * penetration
-	var relative_vel := spring_up_dir.dot(velocity)
+	var relative_vel := spring_up_direction.dot(velocity)
 	var spring_damp_force := spring_damping * relative_vel
-	return (spring_force - spring_damp_force) * get_collision_normal()
+	var force_vector := (spring_force - spring_damp_force) * get_collision_normal()
+	return force_vector
+
+
+func get_pedal_force(pedal_input: float) -> Vector3:
+	if pedal_input > 0.0:
+		var foward_direction := -global_basis.z
+		var contact := get_collision_point()
+		var force_vector := foward_direction * pedal_force * pedal_input
+		return force_vector
+	else: return Vector3(0, 0, 0)
+
+# Works now, might be a problem later with all the easing to a stop
+func get_brake_force(brake_input: float, velocity: Vector3) -> Vector3:
+	if brake_input > 0.0:
+		var forward_direction := -global_basis.z
+		var relative_velocity := forward_direction.dot(velocity)
+		
+		# No velocity = No brake force
+		if is_zero_approx(relative_velocity):
+			return Vector3(0, 0, 0)
+		
+		# get direction of brake force
+		var force_direction := forward_direction
+		force_direction *= -sign(relative_velocity)
+		
+		# eases into a stop
+		if absf(relative_velocity) < 0.5:
+			return force_direction * relative_velocity
+		
+		var force_vector: Vector3
+		
+		# if velocity is low enough apply brake as damping
+		if absf(relative_velocity) < 3:
+			force_vector = force_direction * relative_velocity * (brake_force * brake_input)
+		
+		# else apply brake force normally
+		force_vector = force_direction * brake_force * brake_input
+		return force_vector
+	
+	else: return Vector3(0, 0, 0)
+
+
+# TODO move cornering stiffness or implement new tire model
+# TODO can't steering while in air because this only gets called when contacting the ground
+func get_steering_force(steering_input: float, velocity: Vector3) -> Vector3:
+	var cornering_stiffness := 100.0 # magic cornering coefficient - N per rad slip angle
+	
+	# Rotating the wheel to steering_input
+	var degrees_to_rotate := (max_steering_angle * steering_input) - current_steering_angle
+	current_steering_angle += degrees_to_rotate
+	global_basis = global_basis.rotated(steering_axis, deg_to_rad(-degrees_to_rotate))
+	
+	# get forward and side vectors to calculate slip angle
+	var forward_direction := -global_basis.z
+	var side_direction := global_basis.x
+	var ground_normal := get_collision_normal()
+	
+	# project the forward and side vectors onto the ground normal
+	forward_direction = (forward_direction - forward_direction.project(ground_normal)).normalized()
+	side_direction = (side_direction - side_direction.project(ground_normal)).normalized()
+	
+	var slip_angle := calculate_slip_angle(velocity, forward_direction, side_direction)
+	
+	var steering_force := side_direction * cornering_stiffness * slip_angle
+	#DebugDraw3D.draw_arrow_ray(global_position, forward_direction, 1.0, Color.BLUE, 0.1)
+	#DebugDraw3D.draw_arrow_ray(global_position, side_direction, 1.0, Color.GREEN, 0.1)
+	DebugDraw3D.draw_arrow_ray(global_position, steering_force, 1.0, Color.RED, 0.1)
+	#DebugDraw3D.draw_arrow_ray(global_position, velocity.normalized(), 1.5, Color.PURPLE, 0.1)
+	#print("slip_angle ", slip_angle, " steering ", steering_force)
+	return steering_force
+
+
+func calculate_slip_angle(velocity: Vector3, forward_direction: Vector3, side_direction: Vector3) -> float:
+	# get forward and side velocity components
+	var lateral_velocity := side_direction.dot(velocity)
+	var longitudinal_velocity := forward_direction.dot(velocity)
+	
+	# zero floats if approx zero to remove force error
+	if abs(longitudinal_velocity) < 0.1:
+		return 0.0
+	
+	var slip_angle := -atan2(lateral_velocity, longitudinal_velocity)
+	return slip_angle
+
+
+func set_steering_axis(angle: float) -> void:
+	steering_axis = Vector3(0.0, sin(angle), -cos(angle)).normalized()
